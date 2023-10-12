c5a1bd2e41c1602bbf482cd27c9938c7342758b3
fadafful <fadafful@gmail.com>
2023-10-12 15:56:25 +0000

    refresh token added

--- src/config/axiosConfig.js
+++ src/config/axiosConfig.js
@@ -1,8 +1,35 @@
 import axios from "axios";
+import { refreshToken } from "../services/authServices";
+
 
 const instance = axios.create({
-  baseURL: "https://293cec0d-e469-4a2a-9169-928c627f4a60.mock.pstmn.io/api/v1",
+  baseURL: process.env.REACT_APP_API_URL,
   headers: { "Content-Type": "application/json" }
 });
+
+instance.interceptors.request.use(function (config) {
+  const token = localStorage.getItem("accessToken");
+  config.headers.Authorization = `Bearer ${token}`;
+  return config;
+});
+
+instance.interceptors.response.use(
+  (response) => {
+    return response;
+  },
+  (error) => {
+    const originalRequest = error.config;
+    if (error.response.status === 401 && !originalRequest._retry) {
+      originalRequest._retry = true;
+      return refreshToken().then(() => {
+        originalRequest.headers.Authorization = `Bearer ${localStorage.getItem(
+          "accessToken"
+        )}`;
+        return instance(originalRequest);
+      });
+    }
+    return Promise.reject(error);
+  }
+);
 
 export default instance;
--- src/services/authServices.js
+++ src/services/authServices.js
@@ -20,3 +20,27 @@ export const logout = async () => {
     console.log("Error: ", error);
   }
 };
+
+export const refreshToken = async () => {
+  try {
+    let refreshToken = localStorage.getItem("refreshToken");
+
+    const { data } = await axios.post("/auth/refresh", {
+      refreshToken: refreshToken
+    });
+
+    localStorage.setItem("accessToken", data.access_token);
+    localStorage.setItem("refreshToken", data.refresh_token);
+  } catch (err) {
+    console.error(err);
+  }
+};
+
+export const testRequest = async () => {
+  try {
+    const response = await axios.post("/asdasdasd");
+    return response.data;
+  } catch (error) {
+    console.log("Error: ", error);
+  }
+};
